61d9873bbd526c4a727df2ec0a506e73774e9c55
Yash <yash@mediaamp.co.in>
2024-01-25 12:02:14 +0530

    New footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,111 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
 
-import {Row, Col, Button, Form} from 'react-bootstrap';
+import { Row, Col, Button, Form } from "react-bootstrap";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FaFacebookF, FaTwitter, FaGoogle, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
-
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <Navbar fixed="bottom" className='bg-black align-items-center justify-content-center' >
-
-      <footer className='text-center' style={{ backgroundColor: 'dark', color: 'white' }}>
-      
-        <Container className='p-1'>
-
+    <Navbar
+      fixed="bottom"
+      className="bg-light align-items-center justify-content-center"
+    >
+      <footer className="text-center">
+        <Container className="p-1">
           <div>
-          
-            <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a
+              href="https://github.com/your-github-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
               <FaGithub className="m-2" />
             </a>
-            <a href="https://www.linkedin.com/in/your-linkedin-username" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a
+              href="https://www.linkedin.com/in/your-linkedin-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
               <FaLinkedin className="m-2" />
             </a>
-            <a href="https://www.facebook.com/your-facebook-username" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a
+              href="https://www.facebook.com/your-facebook-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
               <FaFacebookF className="m-2" />
             </a>
-            <a href="https://twitter.com/your-twitter-username" target="_blank"rel="noopener noreferrer" className="text-white">
+            <a
+              href="https://twitter.com/your-twitter-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
               <FaTwitter className="m-2" />
             </a>
-            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a
+              href="https://www.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
               <FaGoogle className="m-2" />
             </a>
-            <a href="https://www.instagram.com/your-instagram-username" target="_blank" rel="noopener noreferrer" className="text-white">
-              <FaInstagram className="m-2" />  
+            <a
+              href="https://www.instagram.com/your-instagram-username"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black"
+            >
+              <FaInstagram className="m-2" />
             </a>
-
           </div>
 
-          <section className=''>
+          <section className="">
             <Form>
-              <Row className='d-flex justify-content-center align-items-center'>
+              <Row className="d-flex justify-content-center align-items-center">
                 <Col size="auto">
-                  <p className='pt-2'>
+                  <p className="pt-2">
                     <strong>Sign up for our newsletter</strong>
                   </p>
                 </Col>
 
-                <Col md='5' start>
-                  <Form.Group className='mb-4'>
-                    <Form.Control type='email' placeholder='Email address' />
+                <Col md="5" start>
+                  <Form.Group className="mb-4">
+                    <Form.Control type="email" placeholder="Email address" />
                   </Form.Group>
                 </Col>
 
                 <Col size="auto">
-                  <Button variant='outline-light' type='submit' className='mb-4'>
+                  <Button variant="outline-dark" type="submit" className="mb-4">
                     Subscribe
                   </Button>
                 </Col>
               </Row>
             </Form>
           </section>
-
         </Container>
 
-        <div className='text-center p-1' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2020 Copyright:
-          <a className='text-white' href='https://mdbootstrap.com/'>
+        <div
+          className="text-center p-1"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2024 Copyright :&nbsp;
+          <a className="text-dark" href="https://google.com/">
             MediaAmp.co.in
           </a>
         </div>
-
       </footer>
-
     </Navbar>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Navbar, Nav, NavbarText, Button, Col } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavbarText,
+  Button,
+  Col,
+  Container,
+} from "react-bootstrap";
 import { faCog, faUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Offcanvas from "react-bootstrap/Offcanvas";
@@ -21,26 +28,20 @@ function Header() {
   };
 
   return (
-    <Navbar
-      className=" bg-light p-0 d-flex align-items-center justify-content-between"
-      expand="lg"
-    >
-      <div className="d-flex align-items-center">
-      
-        {/* Navbar brand (logo) */}
-        <Navbar.Brand href="/">
-          <img
-            src="/timesheets_logo.png"
-            style={{ width: "100%", maxWidth: "120px" }}
-            alt=""
-          />
-        </Navbar.Brand>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <div className="d-flex ">
+          {/* Navbar brand (logo) */}
+          <Navbar.Brand href="/">
+            <img
+              src="/timesheets_logo.png"
+              style={{ width: "100%", maxWidth: "120px" }}
+              alt=""
+            />
+          </Navbar.Brand>
 
-        <div>
-          {/* Navbar items */}
-
+          {/* Dropdown */}
           <Nav className="ml-auto">
-            {/* Dropdown */}
             <Button variant="" onClick={handleShow} size="lg">
               <FontAwesomeIcon icon={faBars} />
             </Button>
@@ -50,29 +51,31 @@ function Header() {
             </Offcanvas>
           </Nav>
         </div>
-      </div>
 
-      <div>
-        {/* Navbar text */}
-        <NavbarText className="fs-4">Timesheet Management System</NavbarText>
-      </div>
+        {/* Text */}
+        <div className="">
+          <NavbarText className="text-center text-md-left fs-4 fs-md-3 fs-lg-2 d-none d-md-block">
+            Timesheet Management System
+          </NavbarText>
+        </div>
 
-      <div>
-        {/* Settings and Profile links */}
-        <Col
-          xs={12}
-          md={6}
-          lg={4}
-          className="d-flex align-items-center justify-content-end"
-        >
-          <Nav.Link href="" className="m-2 p-1" onClick={handleSettings}>
-            <FontAwesomeIcon icon={faCog} size="lg" />
-          </Nav.Link>
-          <Nav.Link href="" className="p-1" onClick={handleProfileview}>
-            <FontAwesomeIcon icon={faUser} size="lg" />
-          </Nav.Link>
-        </Col>
-      </div>
+        <div>
+          {/* Settings and Profile links */}
+          <Col
+            xs={12}
+            md={6}
+            lg={4}
+            className="d-flex align-items-center justify-content-end"
+          >
+            <Nav.Link href="" className="m-2 p-1" onClick={handleSettings}>
+              <FontAwesomeIcon icon={faCog} size="lg" />
+            </Nav.Link>
+            <Nav.Link href="" className="p-1" onClick={handleProfileview}>
+              <FontAwesomeIcon icon={faUser} size="lg" />
+            </Nav.Link>
+          </Col>
+        </div>
+      </Container>
     </Navbar>
   );
 }
